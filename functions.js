9b6b129fae0ebe0ab85f0ede58befbcd50dd2e0c
freakie123 <ralphmensah123@gmail.com>
2019-08-20 11:42:41 +0000

    adding functions

---
+++ functions.js
@@ -0,0 +1,38 @@
+function hello1(){
+  console.log('world!')
+}
+hello1()
+
+function hello2(name) {
+  console.log(`Hello ${ name }`);
+}
+
+hello2("Raphael");
+
+function hello3(name, country  = 'Ghana') {
+  console.log(`Hello ${ name } from ${ country }`);
+}
+
+hello3(`ralph`);
+
+
+function printDetails(person) {
+  const age = (person.age ? person.age : 0);//if (person.age === person.age){}
+  const country = (person.country ? person.country : 'Ghana');//returns truthy thus Ghana
+
+  console.log(`Hello ${ person.name } from ${ country } with age ${ age } years`);
+}
+
+printDetails({ name: 'Raphael', age: '24' });
+
+// Using return keyword
+function printDetails1(person) {
+  const age = (person.age ? person.age : 0)
+  const country = (person.country ? person.country : 'Ghana')
+
+  return `Hello ${ person.name } from ${ country } with age ${ age } years`;
+  //after a return statement nothing is added
+}
+
+const message = printDetails1({ name: 'Raphael', age: '24' });
+console.log(message)
